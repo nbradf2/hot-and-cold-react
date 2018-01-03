c1859e42eaf249ec03a3771874ec040f4d5ef93c
Nicole Bradford <nbradf2@gmail.com>
2018-01-02 18:52:06 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+	<HotorCold />,
+	document.getElementById('root')
+);
+
+// MODULES
+// top nav bar
+// form to enter guess and submit button
+// counter to keep track of how many guesses
+// section to display 
